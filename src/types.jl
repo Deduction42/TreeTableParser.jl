b874d1341254ee3c7930378caebbc7deefd7f5e9
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2026-06-03 10:13:27 -0600

    Granular keys and children protocol

--- src/types.jl
+++ src/types.jl
@@ -12,16 +12,57 @@ Tables.getcolumn(row::AbstractTreeRow, ii::Int64) = getfield(row, ii)
 Tables.getcolumn(row::AbstractTreeRow, fn::Symbol) = getfield(row, fn)
 
 #Forward Fill option on missing 
-function (::Type{R})(mergefunc, row::AbstractRow, init::AbstractAtomicRow) where R<:AbstractAtomicRow 
-    return R(map(fn->mergefunc(fn, row, init), fieldnames(R))...)
+function (::Type{R})(fillfunc, row::AbstractRow, init::AbstractAtomicRow) where R<:AbstractAtomicRow 
+    return R(map(fn->fillfunc(fn, row, init), fieldnames(R))...)
 end
 
 function (::Type{R})(row::AbstractRow, init::AbstractAtomicRow) where R<:AbstractAtomicRow 
-    return R(default_merger, row, init)
+    return R(forward_fill, row, init)
 end
 
 
-function default_merger(fn::Symbol, row::AbstractRow, init::R) where R <: AbstractAtomicRow 
+#Default options for getting ids and children 
+"""
+    key_field(row::AbstractTreeRow)
+
+Gets the row's identifier field which by default is `:__id__`. If your row object has a different identifier field name 
+(it usually will, because it's specified by the source table file) overload this function for that specific row type.
+"""
+key_field(row::AbstractTreeRow) = :__id__
+
+
+"""
+    child_field(row::AbstractParentRow)
+
+Gets the row's child field which by default is `:__children__`. If your row object type has a different child field name,
+overload this function for that specific type
+"""
+child_field(row::AbstractParentRow) = :__children__
+
+"""
+    get_key(row::AbstractTreeRow) 
+
+Gets the key for `row` (overload `key_field(row)` to change what field the key is in)
+"""
+get_key(row::AbstractTreeRow) = getfield(row, key_field(row))
+
+"""
+    get_children(row::AbstractParentRow)
+
+Gets the children for `row` (overload `child_field(row)` to change what field the children are in)
+"""
+get_children(row::AbstractParentRow) = getfield(row, child_field(row))
+
+"""
+    forward_fill(fn::Symbol, row::AbstractRow, init::R) where R <: AbstractAtomicRow 
+
+Produces a converted element for fieldnbame `fn` given by `row` in the appropriate type. If `row[fn]` is `Misisng` or `Nothing`
+and if the field type `fn` doesn't support it, the corresponsing field of `init` will be used (i.e. imputing by forward-fill).
+If forward-fill behaviour isn't desired for a particular field, make sure it supports any missing values that could be in `row` 
+(usually `Union{T, Missing}`). Other rules can replace this, for example, if the `row` type uses empty String to represent a missing value,
+extend a `forward_fill` rule for that specific row type.
+"""
+function forward_fill(fn::Symbol, row::AbstractRow, init::R) where R <: AbstractAtomicRow 
     rowval = row[fn]
     try 
         return convert(fieldtype(R, fn), rowval)
